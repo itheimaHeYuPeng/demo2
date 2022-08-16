b11f07070204cf8fdbd489d0941a56863811ceb6
heyupeng <2848954798@qq.com>
2022-08-16 20:33:11 +0800

    payment commit of code

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("i love java");
+        System.out.println("payment of update code");
     }
 }
