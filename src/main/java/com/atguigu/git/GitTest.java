f42598aa6bdb3332d3694e088f70ce88c372b945
heyupeng <2848954798@qq.com>
2022-08-16 19:45:11 +0800

    first commit!!!

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author heyupeng
+ * @create 2022-08-16 19:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
