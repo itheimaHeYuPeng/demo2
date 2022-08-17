02d8d70b2b3caaf4a9206b24311bbd92bf4c810c
heyupeng <2848954798@qq.com>
2022-08-17 14:40:28 +0800

    push test。。。。

---
+++ src/main/java/com/atguigu/git/PushTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author heyupeng
+ * @create 2022-08-17 14:36
+ */
+public class PushTest {
+    public static void main(String[] args) {
+        System.out.println("开始push代码到GitHub远程库....");
+    }
+}
