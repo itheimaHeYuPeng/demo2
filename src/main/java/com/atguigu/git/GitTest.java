741edc0abb9dfd06180393912848b95a5e4097d4
heyupeng <2848954798@qq.com>
2022-08-17 08:48:32 +0800

    master commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("i love java");
         System.out.println("payment of update code");
+        System.out.println("master commit");
     }
 }
